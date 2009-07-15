ed00a35e8f043e59bfb38ac82766cb4004c92ab3
danmaclean <maclean.daniel@gmail.com>
2009-07-15 11:16:51 +0100

    added methods for db and PhosPhat style modified peptide strings

--- Sequence.pm
+++ Sequence.pm
@@ -74,7 +74,58 @@ my %residue_posns;
 return %residue_posns;
 
 }
+sub get_phossite_positions_specific_and_ambiguous{
 
+my $protein = shift;
+my $best_guess = shift;
+my $pep = $best_guess;
+my $pep_start = shift;
+my $pep_end = shift;
+#my $specific_residue = shift; ### if this is set will return phos posns only for one type of residue, options are S, T, Y and ALL
+
+#if (!$specific_residue or $specific_residue =~ m/all/i){
+$best_guess =~ s/\(/[/g;
+$best_guess =~ s/\)/]/g;
+$best_guess =~ s/\[s\]/s/gi;
+$best_guess =~ s/\[t\]/t/gi;
+$best_guess =~ s/\[y\]/y/gi;
+$best_guess =~ s/\[ox/[/g;
+$best_guess =~ s/\[\*/\[/g;
+my %phos_site_posns;
+
+		while($best_guess =~ m/(\[p)/g){
+			my $match = $`; #`
+			my $pos = length($match);
+			$best_guess =~ s/\[p//;
+#			$best_guess =~ s/\[//;
+			$best_guess =~ s/\]//;
+			#warn "added $pos\n";
+			$pos += $pep_start;
+
+			$phos_site_posns{'specific'}{$pos} = 1; 
+			
+		}
+$pep =~ s/\(/[/g;
+$pep  =~ s/\)/]/g;
+$pep =~ s/\[pS\]/S/gi;
+$pep =~ s/\[pT\]/T/gi;
+		       $pep =~ s/\[pY\]/Y/gi;
+		       warn $pep, "\n";
+		while($pep =~ m/(\[)/g){
+		        my $match = $`; #`
+			my $pos = length($match);
+#			$best_guess =~ s/\[p//;
+			$pep =~ s/\[//;
+			$pep =~ s/\]//;
+			#warn "added $pos\n";
+			$pos += $pep_start;
+
+			$phos_site_posns{'ambiguous'}{$pos} = 1; 
+			
+		    }
+
+return %phos_site_posns;
+ }
 sub get_phossite_positions{
 
 my $protein = shift;
